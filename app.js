9f02334e1a52321242b990a216b269ecc0403957
Segun <shegunolanitori@gmail.com>
2020-08-15 16:49:18 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const readline = require('readline')
+const fs = require("fs")
+let rl = readline.createInterface(process.stdin, process.stdout)
+
+console.log(`This app collects information about your project and displays it in a README.md
+
+`);
+
+let questions = ["Describe your project in two sentences: ", "Installation", "Usage"]
+
+console.log(`Question 1:
+
+`);
+
+
+rl.question(questions[0], (answer) => {
+    fs.writeFileSync('README.md',
+        `# Project Description
+    
+### ${answer}`)
+    rl.close();
+})
+
+
+rl.on("close", () => {
+    console.log(`answer recieved, check the generated README in this directory`);
+    process.exit(0)
+})
